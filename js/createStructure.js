efc7d9cf3fa1ae8853dc43abc3c9c8c3d54b856c
Rostyslav <novijrost@gmail.com>
2022-12-24 04:22:35 +0200

    add new start

---
+++ js/createStructure.js
@@ -0,0 +1,7 @@
+let startCraft =  {
+
+ 
+
+}
+
+export let createStructure = startCraft
---
+++ js/script.js
@@ -0,0 +1,7 @@
+import {createStructure} from './createStructure.js'
+
+(() => {
+    
+    createStructure ()
+
+})();
